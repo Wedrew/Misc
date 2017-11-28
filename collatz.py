bf8e1c60ad9a23a00d002770fda2d3fdb5dd3057
Andrew Pagan <apagan1@umbc.edu>
2017-11-27 21:43:26 -0500

    Change code to answer project euler

--- collatz.py
+++ collatz.py
@@ -8,7 +8,15 @@ def collatz_sequence(x):
          x = x / 2
        else:
          x = 3 * x + 1 
-       seq.append(x)    # Added line
+       seq.append(int(x))    # Added line
     return seq
 
-print(collatz_sequence(27))
+size = input("Please enter size: ")
+toplength = 0
+
+for x in range(1,int(size)+1):
+  if len(collatz_sequence(x)) > toplength:
+    toplength = len(collatz_sequence(x))
+    num = x
+
+print("Greatest orbit from 1 to " + str(x) + " is " + str(toplength) + " coming from number " + str(num))
